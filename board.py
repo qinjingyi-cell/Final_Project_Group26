c7440b759593a0510ea588dfcd840c642932f283
unknown <qin.jingyi@ufl.edu>
2026-04-14 23:18:16 -0400

    Some changes to cell and board class. Feel free to change anything!

--- board.py
+++ board.py
@@ -1,2 +1,66 @@
+import pygame
+from cell import Cell
+from sudoku_generator import SudokuGenerator
+
 class Board:
-    pass
+    def __init__(self, width, height, screen, difficulty):
+        self.width = width
+        self.height = height
+        self.screen = screen
+
+        if difficulty == 'easy':
+            self.removed_cell = 30
+        elif difficulty == 'medium':
+            self.removed_cell = 40
+        else:
+            self.removed_cell = 50
+
+        self.original_board = SudokuGenerator(9, self.removed_cell)
+        self.board = SudokuGenerator(9, self.removed_cell)
+
+
+
+    def draw(self):
+        self.screen = pygame.display.set_mode((540, 540))
+        self.screen.fill("lightblue")
+
+
+    def select(self, row, col):
+        pass
+
+
+    def click(self, x, y):
+        pass
+
+
+    def clear(self):
+        pass
+
+
+    def sketch(self, value):
+        pass
+
+
+    def place_number(self, value):
+        pass
+
+
+    def reset_to_original(self):
+        pass
+
+
+    def is_full(self):
+        pass
+
+
+    def update_board(self):
+        pass
+
+
+    def find_empty(self):
+        pass
+
+
+    def check_board(self):
+        pass
+
--- cell.py
+++ cell.py
@@ -1,14 +1,22 @@
+import pygame
+
 class Cell:
     def __init__(self, value, row, col, screen):
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.sketched_value = None
 
 
     def set_cell_value(self, value):
-        pass
+        self.value = value
+        self.sketched_value = None
 
 
     def set_sketched_value(self, value):
-        pass
+        self.sketched_value = value
+
 
     def draw(self):
         pass
